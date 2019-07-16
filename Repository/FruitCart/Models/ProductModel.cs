4db55feeca1beda6642b54c1b9ef591c33c3abaf
SreedeviRamanujan <52950612+SreedeviRamanujan@users.noreply.github.com>
2019-07-16 11:48:40 +0100

    Updating the model with missing code

--- Repository/FruitCart/Models/ProductModel.cs
+++ Repository/FruitCart/Models/ProductModel.cs
@@ -15,6 +15,12 @@ namespace FruitCart.Models
             DataModel dataModel = new DataModel();
             this.Products = dataModel.Products.ToList();
             }
+        //Find all products 
+        public List<Product> findAll()
+        { return this.Products; }
+        //Find a specific Product
+        public Product find(int Id)
+        { return this.Products.Single(p => p.Id.Equals(Id)); }
 
        
     }
